0550221df039525daaab157213d6092701c90b12
Tolgar32 <99904992+Tolgar32@users.noreply.github.com>
2022-12-18 22:58:45 +0000

    Laba 6

---
+++ LabaOS/OS_Lab_6/FixedSections.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_Practice_6
+{
+    class FixedSections
+    {
+        public int SectionsCount { get; set; }
+        public List<int> SectionsSize { get; set; }
+        private char ID { get; set; }
+        private List<OS_Address> AdrTable { get; set; }
+
+        string OZU;
+        public FixedSections(int count, List<int> sizes)
+        {
+            SectionsSize = new();
+            AdrTable = new();
+            ID = 'A';
+            for (int i = 0; i < count; i++)
+            {
+                try
+                {
+                    SectionsSize.Add(sizes[i]);
+
+                } 
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            while (true)
+            {
+                Draw();
+            }
+        }
+
+        private string PeekAddress(float adr)
+        {
+            foreach (OS_Address item in AdrTable)
+            {
+                if (item.Address <= adr &&
+                    item.Address + item.Task.Size >= adr)
+                {
+                    return item.Task.Identificator.ToString();
+                }
+            }
+            return " ";
+        }
+
+        private bool FreeAddress(float adrStart, float adrFinish)
+        {
+            foreach (OS_Address item in AdrTable)
+            {
+                if (item.Address < adrFinish &&
+                    item.Address + item.Task.Size > adrStart)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void ComposeOZU()
+        {
+            OZU = "";
+            for (int j = 0; j < 65536; j++)
+            {
+                OZU += PeekAddress(j);
+            }
+            System.IO.File.WriteAllText("ОЗУ", OZU);
+        }
+
+        private void Draw()
+        {
+            Console.Clear();
+            Console.WriteLine("┌─ Фиксированные разделы ────────────────────────────┐");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("│ ");
+                for (int j = 0; j < 50; j++)
+                {
+                    Console.Write(PeekAddress((j + 50 * i) * 131.072f));
+                }
+                Console.WriteLine(" │");
+            }
+            Console.WriteLine("└────────────────────────────────────────────────────┘");
+            Console.WriteLine(" A - добавить задачу   D - удалить задачу");
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.A:
+                    Console.Write("\n Размер задачи > ");
+                    try
+                    {
+                        AddTask(OS_Inputing.OS_Int(0, OS_Inputing.Infinity));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Write(" Подсистема управления памятью выдала следующую ошибку:\n ");
+                        Console.WriteLine(ex.Message);
+                        Console.ReadKey();
+                    }
+                    break;
+                case ConsoleKey.D:
+                    Console.Write("\n ID задачи > ");
+                    try
+                    {
+                        UnloadTask(Console.ReadLine()[0]);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Write(" Подсистема управления памятью выдала следующую ошибку:\n ");
+                        Console.WriteLine(ex.Message);
+                        Console.ReadKey();
+                    }
+                    break;
+            }
+        }
+
+
+        public void AddTask(int task_size)
+        {
+            int start_address = 0;
+            foreach (int item in SectionsSize)
+            {
+                if (FreeAddress(start_address, start_address + item) && task_size <= item)
+                {
+                    AdrTable.Add(new OS_Address(new OS_Task(ID++, task_size), start_address));
+                    ComposeOZU();
+                    return;
+                } else
+                {
+                    start_address += item;
+                }
+            }
+            throw new Exception("Нет ни одного подходящего раздела");
+        }
+    
+        public void UnloadTask(char id)
+        {
+            foreach (OS_Address section in AdrTable)
+            {
+                if (section.Task.Identificator == id)
+                {
+                    AdrTable.Remove(section);
+                    ComposeOZU();
+                    return;
+                }
+            }
+            throw new Exception("Нет задачи с таким ID");
+        }
+    }
+}
---
+++ LabaOS/OS_Lab_6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OS_Practice_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max_size = 64 * 1024;
+            
+                    Console.Clear();
+                    Console.WriteLine(" 1. Распределение памяти фиксированными разделами\n");
+                    Console.WriteLine(" На сколько разделов вы хотите разделить оперативную память?");
+                    Console.Write(" > ");
+                    int c1 = OS_Inputing.OS_Int(1, OS_Inputing.Infinity);
+                    List<int> sections1 = new();
+                    for (int i1 = 0; i1 < c1; i1++)
+                    {
+                        Console.Write($"  Размер участка №{i1 + 1}> ");
+                        int sect = OS_Inputing.OS_Int(1, max_size);
+                        sections1.Add(sect);
+                        max_size -= sect;
+                    }
+                    sections1.Add(max_size);
+                    _ = new FixedSections(c1, sections1);
+        }
+    }
+}
